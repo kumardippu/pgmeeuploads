2c176a0bae9a5a3fa8310b4929deb1a893a3845f
kumardippu <kumardippu@gmail.com>
2017-06-23 22:55:35 +0530

    initial code

---
+++ www/js/custom/route - Copy.js
@@ -0,0 +1,42 @@
+ // configure our routes
+ //pgmee defined at main.js
+    pgmee.config(['$routeProvider',function($routeProvider) {
+        $routeProvider
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+
+            .when('/login', {
+                templateUrl : 'pages/login.html',
+                controller  : 'loginController'
+            })
+            .when('/logout', {
+                templateUrl : 'pages/login.html',
+                controller  : 'logoutController'
+            })
+            // route for the about page
+            /*.when('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/quiz', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'quizController'
+            })
+           */
+            // route for the contact page
+            .when('/qbank/:catid/', {
+                templateUrl : 'pages/qbank.html',
+                controller  : 'qbankController'
+            })
+            
+            .otherwise({redirectTo : '/'});
+            /*.when('/contact', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'contactController'
+            });*/
+    }]);
